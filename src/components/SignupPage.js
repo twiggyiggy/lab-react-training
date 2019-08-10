b41bb940157dfa163559c950fc649098cbe8edc6
Ignacy Wojdylo <ignacy.wojdylo@gmail.com>
2019-08-10 20:51:19 +0200

    start forms

--- src/components/SignupPage.js
+++ src/components/SignupPage.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react'
-import Bulma from 'bulma'
+import bulma from 'bulma'
 
 export default class SignupPage extends Component {
-
-    state = {
-        email: '',
-        password: '',
-        nationality: ''
+    constructor(props){
+        super(props)
+        this.state = {
+            email: '',
+            password: '',
+            nationality: ''
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
-
-    handleChange(event) {
-        // this.setState({value: event.target.value});
-      }
     
     handleSubmit(event) {
-        // alert('A name was submitted: ' + this.state.value);
+        console.log(event.target.value)
         event.preventDefault();
     }
 
@@ -22,19 +21,19 @@ export default class SignupPage extends Component {
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
-                    <label class="label">E-mail:</label>
-                    <input class="input" value={this.state.email} type="text" placeholder="Enter e-mail address"/>
-                    <label class="label">Password:</label>
-                    <input class="input"  value={this.state.password} type="password" placeholder="Enter password"/>
-                    <label class="label">Nationality:</label>
-                    <div class="select">
+                    <label className="label">E-mail:</label>
+                    <input className="input" type="text" placeholder="Enter e-mail address"/>
+                    <label className="label">Password:</label>
+                    <input className="input" type="password" placeholder="Enter password"/>
+                    <label className="label">Nationality:</label>
+                    <div className="select">
                         <select>
                             <option>English</option>
                             <option>German</option>
                             <option>French</option>
                         </select>
                     </div>
-                    <button type="Submit" value="Submit" class="button is-link">Sign up</button>
+                    <button type="Submit" value="Submit" className="button is-link">Sign up</button>
                 </form>
             </div>
         )
